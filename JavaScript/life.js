8732bc35e7c1d1118e9903bb7df7e27f79879f60
Johannes Maibaum <jmaibaum@googlemail.com>
2015-02-13 19:01:51 +0100

    Add JavaScript version (not yet functional).

---
+++ JavaScript/life.js
@@ -0,0 +1,65 @@
+// Wait for DOM, then initialize.
+document.addEventListener("DOMContentLoaded", init, false);
+
+var cellsPerLine = 20;
+
+// Initialize everything.
+function init()
+{
+    // Prepare canvas:
+    canvas = document.getElementById("board");
+    context = canvas.getContext("2d");
+
+    boardSideLength = canvas.width;  // Assume that canvas is quadratic.
+    boardDivision = Math.floor(boardSideLength / cellsPerLine);
+    cellSize = boardDivision - 2;
+
+    // Draw grid:
+    context.strokeStyle = "#cccccc";
+    context.lineWidth = 2;
+
+    for (var i = 0; i <= boardSideLength; i = i + boardDivision) {
+        context.moveTo(0, i);
+        context.lineTo(boardSideLength, i);
+        context.moveTo(i, 0);
+        context.lineTo(i, boardSideLength);
+        context.stroke();
+    }
+
+    // Add EventListeners:
+    canvas.addEventListener("mousedown", getMousePositionOnCanvas, false);
+}
+
+
+// Callback function to determine the mouse click coordinates on the canvas.
+function getMousePositionOnCanvas(event)
+{
+    var x = event.x;
+    var y = event.y;
+
+    // Firefox workaround:
+    if (x == undefined && y == undefined) {
+        x = event.clientX + document.body.scrollLeft +
+              document.documentElement.scrollLeft;
+        y = event.clientY + document.body.scrollTop +
+              document.documentElement.scrollTop;
+    }
+
+    x -= canvas.offsetLeft;
+    y -= canvas.offsetTop;
+    //console.log("x:" + x + " y:" + y);
+
+    drawCell(x, y);
+}
+
+
+// Draw an actual cell on canvas.
+function drawCell(x, y)
+{
+    var cellx = Math.floor(x / boardDivision) * boardDivision + 1;
+    var celly = Math.floor(y / boardDivision) * boardDivision + 1;
+    //console.log(cellx, celly);
+
+    context.rect(cellx, celly, cellSize, cellSize);
+    context.fill();
+}
