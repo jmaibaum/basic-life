e819b70b8ffb00ed154c4885c82c85b45980d041
Johannes Maibaum <jmaibaum@googlemail.com>
2015-02-14 20:49:13 +0100

    First try in modelling the field behind the visuals (BROKEN).

--- JavaScript/life.js
+++ JavaScript/life.js
@@ -4,10 +4,115 @@
  * Copyright (c) 2015 Johannes Maibaum <jmaibaum@gmail.com>
  */
 
+// Variables and types needed for LIFE:
+var cellsPerLine = 10;
+var randomCells = 1;
+
+var Cell = function () {
+    this.lives = false;
+    this.neighbours = 0;
+}
+
+Cell.prototype.liven = function () {
+    if (!this.lives) {
+        this.lives = true;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+Cell.prototype.kill = function () {
+    if (this.lives) {
+        this.lives = false;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var Field = function (rows, columns) {
+    this.rows  = rows;
+    this.columns = columns;
+    this.livingCells = 0;
+    this.data = [];
+
+    for (var i = 0; i < this.rows + 2; i++) {
+        this.data[i] = [];
+
+        for (var j = 0; j < this.columns + 2; j++) {
+            this.data[i].push(new Cell());
+        }
+    }
+}
+
+Field.prototype.liven = function (row, column) {
+    if (this.data[row][column].liven()) {
+        this.livingCells++;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+Field.prototype.kill = function (row, column) {
+    if (this.data[row][column].kill()) {
+        this.livingCells--;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+Field.prototype.countNeighbours = function (row, column) {
+    if (this.data[row][column].lives) {
+        this.data[row - 1][column - 1].neighbours += 1;
+        this.data[row - 1][column].neighbours     += 1;
+        this.data[row - 1][column + 1].neighbours += 1;
+        this.data[row][column - 1].neighbours     += 1;
+        this.data[row][column + 1].neighbours     += 1;
+        this.data[row + 1][column - 1].neighbours += 1;
+        this.data[row + 1][column].neighbours     += 1;
+        this.data[row + 1][column + 1].neighbours += 1;
+    }
+}
+
+Field.prototype.countAllNeighbours = function () {
+    for (var row = 1; row <= this.rows; row++) {
+        for (var column = 1; column <= this.columns; column++) {
+            this.countNeighbours(row, column);
+        }
+    }
+}
+
+// This is just for debugging.
+Field.prototype.print = function() {
+    var printstring = '';
+
+    for (var row = 0; row < this.rows + 2; row++) {
+        var fieldstring = '';
+        var neighbourstring = '';
+
+        for (var column = 0; column < this.columns + 2; column++) {
+            if (this.data[row][column].lives) {
+                fieldstring += '* ';
+            } else {
+                fieldstring += '  ';
+            }
+
+            neighbourstring += this.data[row][column].neighbours + ' ';
+        }
+        printstring += fieldstring + ' | ' + neighbourstring + '\n ';
+    }
+
+    console.log(printstring);
+    console.log(this.livingCells + ' lebende Zellen.');
+}
+
+
+
 // Wait for DOM, then initialize.
 document.addEventListener("DOMContentLoaded", initialize, false);
-
-var cellsPerLine = 20;
 
 // Initialize everything.
 function initialize()
@@ -20,7 +125,7 @@ function initialize()
     boardDivision = Math.floor(boardSideLength / cellsPerLine);
     cellSize = boardDivision - 2;
 
-    // Draw grid:
+    // Draw cell grid:
     context.strokeStyle = "#cccccc";
     context.lineWidth = 2;
 
@@ -32,8 +137,16 @@ function initialize()
         context.stroke();
     }
 
+    field = new Field(cellsPerLine, cellsPerLine);
 
-    // Add EventListeners:
+    fillWithRandomCells(randomCells);
+
+    field.countAllNeighbours();
+
+    field.print();
+
+
+    // Add event listeners:
     canvas.addEventListener("mousedown", getMousePositionOnCanvas, false);
 }
 
@@ -60,17 +173,67 @@ function getMousePositionOnCanvas(event)
     y -= canvas.offsetTop;
     //console.log("x:" + x + " y:" + y);
 
-    drawCell(x, y);
+    var coords = convertClickToFieldCoordinates(x, y);
+    var row = coords[0];
+    var column = coords[1];
+
+    if (field.liven(row, column)) {
+        drawCell(row, column);
+        field.print();
+    } else {
+        field.kill(row, column);
+        drawCell(row, column, true);
+        field.print();
+    }
 }
 
 
-// Draw an actual cell on canvas.
-function drawCell(x, y)
-{
-    var cellx = Math.floor(x / boardDivision) * boardDivision + 1;
-    var celly = Math.floor(y / boardDivision) * boardDivision + 1;
-    //console.log(cellx, celly);
+function convertClickToFieldCoordinates(x, y) {
+    var row = Math.floor(y / boardDivision);
+    var column = Math.floor(x / boardDivision);
 
-    context.rect(cellx, celly, cellSize, cellSize);
-    context.fill();
+    // Ensure that we get valid coordinates:
+    if (row == 11) {
+        row--;
+    }
+    if (column == 11) {
+        column--;
+    }
+
+    return [row, column];
+}
+
+
+// Draw a cell using field coordinates.
+function drawCell(row, column, dead)
+{
+    var cellx = Math.floor(column * boardDivision) + 1;
+    var celly = Math.floor(row * boardDivision) + 1;
+
+    if (dead) {
+        context.fillStyle = "#ffffff";
+    }
+
+    context.fillRect(cellx, celly, cellSize, cellSize);
+
+    if (dead) {
+        context.fillStyle = "#000000";
+    }
+}
+
+
+function fillWithRandomCells(number)
+{
+    for (var i = 0; i < number; i++) {
+        do {
+            var exists = false;
+            var row    = Math.floor(Math.random() * cellsPerLine) + 1;
+            var column = Math.floor(Math.random() * cellsPerLine) + 1;
+
+            if (field.liven(row, column)) {
+                exists = true;
+                drawCell(row, column);
+            }
+        } while (!exists);
+    }
 }
